96120cf939506a59f6e0e0f9a01d8322bf5cded3
Yauncai <yankho-chantel-maria.saliji@iu-study.org>
2025-07-18 17:33:33 +0200

    writing data to csv

--- attendance.py
+++ attendance.py
@@ -1,7 +1,10 @@
+import csv
 from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
+
+CSV_FILE = 'attendance_data.csv'
 
 @app.route('/')
 def home():
@@ -16,11 +19,13 @@ def form():
         status = request.form['status']  # first-timer or regular
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         
-        # For now, just print the data
-        print(f"{name} {surname} ({status}) checked in at {timestamp} with number {phone}")
+        # write data to csv
+        with open(CSV_FILE, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([name, surname, phone, status, timestamp])
         return f"Thank you for gracing us with your presence, {name}!"
     
     return render_template('form.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5003)
+    app.run(debug=True, host='0.0.0.0', port=5004)
